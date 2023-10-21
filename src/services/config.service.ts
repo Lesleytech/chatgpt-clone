64a8983e4e03b380432b972a6badd111c3f17830
Lafen Lesley <lesleytech6@gmail.com>
2023-10-21 08:54:52 +0300

    chore: remove unused humanloop params

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -1,7 +1,5 @@
 export function getLocalConfig() {
   return {
     API_EP: process.env.REACT_APP_API_EP as string,
-    HUMANLOOP_API_KEY: 'hl_sk_29f6888a76399730e8fc34784a7be5130eb082760d24387b',
-    HUMANLOOP_PROJECT: 'chess-gpt',
   };
 }
